283ff56ecf6f9c120b8c4581449efea74fd95fff
UlisesGascon <ulisesgascongonzalez@gmail.com>
2015-01-19 11:14:27 +0100

    Update savemyday.py
    typo mistake solved

--- savemyday.py
+++ savemyday.py
@@ -4,6 +4,6 @@ target.truncate()
 target.write("[Notes]\n")
 target.write("Directory=c:\\notes\data")
 target.close()
-print "DONE! %s file was restored sucesfully." % filename
+print "DONE! %s file was restored successfully." % filename
 print "You can close this windows now."
 print "Have a nice day!"
