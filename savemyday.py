735cf4b87781b940dc83b92184a53761fe92529e
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2015-01-19 12:23:45 +0100

    kill notes before the modification
    This new lines will kill notes, before the file is modified.

--- savemyday.py
+++ savemyday.py
@@ -1,9 +1,18 @@
+import os
+#kill notes (versions)
+os.system("taskkill  /im notes.exe")
+os.system("taskkill  /im notes2.exe")
+os.system("taskkill  /im nlnotes.exe")
+
+#Rewrite notes.ini
 filename = "notes.ini"
 target = open(filename, 'w')
 target.truncate()
 target.write("[Notes]\n")
 target.write("Directory=c:\\notes\data")
 target.close()
-print "DONE! %s file was restored successfully." % filename
+
+#Confirmation
+print "DONE! %s file was restored sucesfully." % filename
 print "You can close this windows now."
 print "Have a nice day!"
