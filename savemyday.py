59667f8232400f2e3e72bb343f3eff4de8ce110e
UlisesGascon <ulisesgascongonzalez@gmail.com>
2015-01-19 11:12:06 +0100

    Create savemyday.py
    This will truncate and write notes.ini file with two lines only.

---
+++ savemyday.py
@@ -0,0 +1,9 @@
+filename = "notes.ini"
+target = open(filename, 'w')
+target.truncate()
+target.write("[Notes]\n")
+target.write("Directory=c:\\notes\data")
+target.close()
+print "DONE! %s file was restored sucesfully." % filename
+print "You can close this windows now."
+print "Have a nice day!"
